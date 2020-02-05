b135b09275a605bc8aa26c4c3d1316677ea60f0c
picinatto <ederli.picinatto@gmail.com>
2020-02-05 07:52:28 -0300

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,26 @@
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        private Func<FriendOrganizerDbContext> _contextCreator;
+
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
+
+        public IEnumerable<Friend> GetAll()
+        {
+            using (var ctx = _contextCreator())
+            {
+                return ctx.Friends.AsNoTracking().ToList();
+            }
+        }
+    }
+}
