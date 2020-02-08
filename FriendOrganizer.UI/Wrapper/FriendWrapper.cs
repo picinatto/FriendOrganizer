d9197839c2bb7127625ff30e5333c5390d260636
picinatto <ederli.picinatto@gmail.com>
2020-02-08 11:50:45 -0300

    Added the validation error on the form for the property FirstName

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,104 +1,47 @@
 ﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+    public class FriendWrapper : ModelWrapper<Friend>
     {
-        public FriendWrapper(Friend model)
+        public FriendWrapper(Friend model) : base(model)
         {
-            Model = model;
         }
 
-        public Friend Model { get; }
         public int Id { get { return Model.Id; } }
+
         public string FirstName
         {
-            get { return Model.FirstName; }
-            set
-            {
-                Model.FirstName = value;
-                OnPropertyChanged();
-                ValidateProperty(nameof(FirstName));
-            }
-        }
-
-        private void ValidateProperty(string propertyName)
-        {
-            ClearErrors(propertyName);
-            switch (propertyName)
-            {
-                case nameof(FirstName):
-                    if(string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
-                    {
-                        AddError(propertyName, "Robots are not valid friends");
-                    }
-                    break;
-            }
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
         }
 
         public string LastName
         {
-            get { return Model.LastName; }
-            set
-            {
-                Model.LastName = value;
-                OnPropertyChanged();
-            }
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
         }
+
         public string Email
         {
-            get { return Model.Email; }
-            set
-            {
-                Model.Email = value;
-                OnPropertyChanged();
-            }
-        }
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        } 
 
-        private Dictionary<string, List<string>> _errorsByPropertyName
-            = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
+        protected override IEnumerable<string> ValidateProperty(string propertyName)
         {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-            ? _errorsByPropertyName[propertyName]
-            : null;
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-        private void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            switch (propertyName)
             {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-        private void ClearErrors(string propertyName)
-        {
-            if (_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
+                case nameof(FirstName):
+                    if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
+                    {
+                        yield return "Robots are not valid friends";
+                    }
+                    break;
             }
         }
     }
---
+++ FriendOrganizer.UI/Wrapper/ModelWrapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class ModelWrapper<T> : NotifyDataErrorInfoBase
+    {
+        public ModelWrapper(T model)
+        {
+            Model = model;
+        }
+
+        public T Model { get; }
+
+        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName]string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(Model, value);
+            OnPropertyChanged(propertyName);
+            ValidatePropertyInteral(propertyName);
+        }
+
+        protected virtual TValue GetValue<TValue>([CallerMemberName]string propertyName = null)
+        {
+            return (TValue)typeof(T).GetProperty(propertyName).GetValue(Model);
+        }
+
+        private void ValidatePropertyInteral(string propertyName)
+        {
+            ClearErrors(propertyName);
+
+            var errors = ValidateProperty(propertyName);
+            if (errors!=null)
+            {
+                foreach(var error in errors)
+                {
+                    AddError(propertyName, error);
+                }
+            }
+        }
+
+        protected virtual IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            return null;
+        }
+    }
+}
